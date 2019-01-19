0a753dc399a9153a659534e1c9d4c6bf3f979318
Ramones13 <gn0m_13@mail.ru>
2019-01-19 22:55:46 +0300

    create Calculate

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,17 @@
 public class Calculate {
 	public static void main(String[] args) {
+		// йа типа начал изучать зачем то Java
+
 		System.out.println("Hell the world!");
+
+		double param1 = 0.0;
+
+		int value1 = Integer.valueOf(args[0]);
+		int value2 = Integer.valueOf(args[1]);
+
+		int summ = value1 + value2;
+
+		System.out.println("Sum = " + summ);
+
 	}
 }
